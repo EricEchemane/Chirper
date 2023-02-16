2c344144186e480720db880d26db91b368f06298
Eric Echemane <eechemane29@gmail.com>
2023-02-16 09:06:01 +0800

    added email notification

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -14,6 +15,10 @@ class Chirp extends Model
         'message',
     ];
 
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+ 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
